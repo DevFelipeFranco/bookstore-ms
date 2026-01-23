ed9a9278055f3503ff1194966863c90dfa2ee4e3
FelipeFranco <devfelipefranco@gmail.com>
2026-01-22 23:23:05 -0500

    Se crea base de datos en memoria

--- customer-ms/application/src/main/java/com/ms/cloud/CustomerMsApplication.java
+++ customer-ms/application/src/main/java/com/ms/cloud/CustomerMsApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-//@Import({
-//        com.ms.cloud.service.CustomerService.class,
-//        com.ms.cloud.out.CustomerPersistenceAdapter.class
-//})
+// @Import({
+// com.ms.cloud.service.CustomerService.class,
+// com.ms.cloud.out.CustomerPersistenceAdapter.class
+// })
 public class CustomerMsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerMsApplication.class, args);
     }
+
+    @org.springframework.context.annotation.Bean
+    public org.springframework.boot.web.servlet.ServletRegistrationBean<jakarta.servlet.Servlet> h2Console() {
+        org.springframework.boot.web.servlet.ServletRegistrationBean<jakarta.servlet.Servlet> bean = new org.springframework.boot.web.servlet.ServletRegistrationBean<>(
+                new org.h2.server.web.JakartaWebServlet());
+        bean.addUrlMappings("/h2-console/*");
+        return bean;
+    }
+
 }
